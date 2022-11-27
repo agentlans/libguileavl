6ee83f2a84c449b015cf33b3f555597867e5c46a
agentlans <at92001@gmail.com>
2022-11-27 03:38:43 -0500

    Finish package

--- Makefile
+++ Makefile
@@ -1,9 +1,27 @@
+GUILE_FLAGS=`pkg-config --cflags --libs guile-3.0`
+EXTENSIONS_DIR=`pkg-config guile-3.0 --variable=extensiondir`
+SITE_DIR=`guile -c '(display (%site-dir))'`
+
 CFLAGS=-O3
+SO_FILE=libguileavl.so
+NAME=avl
 
-all: libguileavl.so
+.PHONY: all clean install
 
-libguileavl.so: guile-avl.c guile-bst.c common.c init.c pavl.c pbst.c list_to_avl.c helper.c
+all: $(SO_FILE)
+
+$(SO_FILE): guile-avl.c common.c init.c pavl.c list_to_avl.c helper.c
 	$(CC) $(CFLAGS) -g -fPIC -shared -o $@ $^ `pkg-config --cflags --libs guile-3.0`
 
+install: $(SO_FILE) $(NAME).scm
+	cp $^ $(EXTENSIONS_DIR)
+	mkdir -p $(SITE_DIR)/extra
+	cp $(NAME).scm $(SITE_DIR)/extra
+
+uninstall:
+	rm -f $(EXTENSIONS_DIR)/$(SO_FILE)
+	rm -f $(SITE_DIR)/extra/$(NAME).scm
+
 clean:
-	rm -f *.so *.o
+	rm -f *.o *.so
+
---
+++ avl.scm
@@ -0,0 +1,43 @@
+(define-module (extra avl)
+  #:use-module (system foreign)
+  #:use-module (system foreign-library)
+  #:export (avl
+	    avl-copy
+	    avl-count
+	    avl-find
+	    avl-insert!
+	    avl-delete!
+	    avl-replace!
+	    avlt
+	    avlt-valid?
+	    avlt-set-first!
+	    avlt-set-last!
+	    avlt-copy
+	    avlt-find
+	    avlt-insert!
+	    avlt-next!
+	    avlt-prev!
+	    avlt-get
+	    avlt-set!
+	    avl-from-array
+	    avl-array))
+
+(load-extension "libguileavl" "init_guile_trees")
+
+(define (avlt-valid? it)
+  "Whether iterator refers to an actual Scheme value."
+  (not (equal? (avlt-get it) %null-pointer)))
+
+(define (avl-array tree)
+  "Returns array of elements in the tree in sorted order."
+  (let* ((n (avl-count tree))
+         (out (make-array #f n))
+         (it (avlt tree)))
+    (avlt-set-first! it tree)
+    ;; For indices i from 0 below n do
+    (do ((i 0 (+ i 1)))
+	((= i n))
+      (begin
+	(vector-set! out i (avlt-get it))
+	(avlt-next! it))) ; advance iterator
+    out))
